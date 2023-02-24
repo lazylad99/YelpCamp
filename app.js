2ad45bf4529380433a25c72232db3bda1477ae7f
Alok Deep <alokdeep9925@gmail.com>
2023-02-24 18:23:09 +0530

    second commit

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const reviewRoutes = require('./routes/reviews');
 
 const MongoStore = require('connect-mongo');
 
-const dbUrl = 'mongodb://127.0.0.1:27017/yelp-camp';
+const dbUrl = process.env.DB_URL;
 mongoose.set('strictQuery', true);
 
 mongoose.connect(dbUrl, {
